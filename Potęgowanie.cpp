72a1784d6091ddcdc78fc3b89d9cf19ad462bd3b
micha <micha@LAPTOP-7VG917GC>
2022-03-10 21:34:44 +0100

    Dodaj pliki projektów.

---
+++ Potęgowanie.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void exit()
+{
+	cout << endl << "Press any key to exit" << endl;
+
+	getchar();
+	getchar();
+	exit(0);
+}
+
+int Multiplying(int p, int w)
+{
+	if (w == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return p * Multiplying(p, w - 1);
+	}
+}
+
+int main()
+{
+	int base, exponent;
+
+	cin >> base;
+	cin >> exponent;
+
+	cout << Multiplying(base, exponent);
+
+	exit();
+}
